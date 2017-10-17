c49914313c032f705c66dee5e99941bc88f8fe5c
Tom Wiesing <tkw01536@gmail.com>
2017-10-17 09:31:33 +0200

    Add requirements to setup.py
    The `setup.py` file did not contain any installation requirements. This
    commit adds `jupyter_client`, `IPython` and `ipykernel` to the
    `install_requires` key, thus ensuring they are available when the
    package is installed.

--- setup.py
+++ setup.py
@@ -12,6 +12,9 @@ setup(
     author='Jupyter Development Team',
     author_email='jupyter@googlegroups.com',
     url='https://github.com/jupyter/echo_kernel',
+    install_requires=[
+        'jupyter_client', 'IPython', 'ipykernel'
+    ],
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
