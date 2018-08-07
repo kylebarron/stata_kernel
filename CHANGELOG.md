400f0a3527a1ae9c065298c7358ec1ad6efd49fa
Kyle Barron <barronk@mit.edu>
2018-08-06 22:31:01 -0400

    Prepare for pip upload

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## [1.1.0] - 2018-08-06
+
+**Initial release!** This would ordinarily be something like version 0.1.0, but the Echo kernel framework that I made this from was marked as 1.1 internally, and I forgot to change that before people started downloading this. I don't want to move my number down to 0.1 and have people who already installed not be able to upgrade.
--- setup.py
+++ setup.py
@@ -3,21 +3,45 @@ from distutils.core import setup
 with open('README.md') as f:
     readme = f.read()
 
+with open('CHANGELOG.md') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'jupyter_client>=5.2.3', 'IPython>=6.5.0', 'ipykernel>=4.8.2',
+    'pexpect>=4.6.0;platform_system=="Darwin"',
+    'pexpect>=4.6.0;platform_system=="Linux"', 'python-dateutil>=2.7.3']
+
+setup_requirements = [
+    'setuptools >= 38.6.0',
+    'twine >= 1.11.0'
+]
+
 setup(
-    name='stata_kernel',
-    version='1.1',
-    packages=['stata_kernel'],
-    description='Stata kernel for Jupyter',
-    long_description=readme,
     author='Kyle Barron',
     author_email='barronk@mit.edu',
-    url='https://github.com/kylebarron/stata_kernel',
-    install_requires=[
-        'jupyter_client>=5.2.3', 'IPython>=6.5.0', 'ipykernel>=4.8.2',
-        'pexpect>=4.6.0;platform_system=="Darwin"',
-        'pexpect>=4.6.0;platform_system=="Linux"', 'python-dateutil>=2.7.3'],
     classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 3'],
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'],
+    description='A Jupyter kernel for Stata. Works with Windows, macOS, and Linux. Preserves program state.',
+    install_requires=requirements,
+    license='GPLv3',
+    long_description=readme + '\n\n' + history,
+    long_description_content_type='text/markdown',
+    keywords='stata',
+    name='stata_kernel',
+    packages=['stata_kernel'],
+    url='https://github.com/kylebarron/stata_kernel',
+    version='1.1.0',
 )
