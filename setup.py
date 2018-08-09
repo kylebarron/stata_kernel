a3d1f6e327c1aa575803a3447cfbb5fb81c5a3cf
Kyle Barron <barronk@mit.edu>
2018-08-09 15:45:50 -0400

    Add pygments as a dependency. Closes #45

--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ with open('CHANGELOG.md') as history_file:
 requirements = [
     'jupyter_client>=5.2.3', 'IPython>=6.5.0', 'ipykernel>=4.8.2',
     'pexpect>=4.6.0;platform_system=="Darwin"',
-    'pexpect>=4.6.0;platform_system=="Linux"', 'python-dateutil>=2.7.3']
+    'pexpect>=4.6.0;platform_system=="Linux"', 'python-dateutil>=2.7.3',
+    'pygments>=2.2.0']
 
 setup_requirements = [
     'setuptools >= 38.6.0',
