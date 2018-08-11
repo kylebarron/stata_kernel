f66f154c0e318d55e5c2bbfea0ef0d47a2774bcc
Mauricio Caceres <mauricio.caceres.bravo@gmail.com>
2018-08-11 15:56:46 -0400

    Still fixing magics.

--- stata_kernel/stata_magics.py
+++ stata_kernel/stata_magics.py
@@ -77,83 +77,85 @@ class StataMagics():
         self.magics_available = {
             'plot': ['plot', 'graph'],
             'exit': ['exit', 'restart'],
-            'globals': ['locals', 'globals']
+            'globals': ['locals', 'globals'],
             'time': ['time', 'timeit']
         }
 
-    def magics(self, code, kernel)
-        quit_early = {'execution_count': count}
-        quit_early['status'] = 'ok'
-        quit_early['payload'] = []
-        quit_early['user_expressions'] = {}
-        magic = self.magic_dict
-
-        if code.strip().startswith("%"):
-            match = magic_regex.match(code.strip())
-            if match:
-                _magic = match.groupdict()['magic']
-                code   = match.groupdict()['code']
-                code   = '' if not code else code.strip()
-
-                if _magic in stata_magics['plot_magics']:
-                    magic['name'] = _magic
-                    try:
-                        args = vars(plot_parser.parse_args(code.split(' ')))
-                        code = ' '.join(args['code'])
-                        args.pop('code', None)
-                        args['width']   = args['scale'] * args['width']
-                        args['height']  = args['scale'] * args['height']
-                        args.pop('scale', None)
-                        magic['kwargs'] = args
-                    except:
-                        magic['name']   = ''
-                        magic['status'] = -1
-
-                elif _magic in stata_magics['global_magics']:
-                    magic['status'] = -1
-                    magic['name']   = _magic
-                    magic['locals'] = (_magic == 'locals')
-                    magic['sregex'] = regex.compile(r"^ {16,16}", flags = regex.MULTILINE)
-                    try:
-                        args = vars(globals_parser.parse_args(code.split(' ')))
-                        code = ' '.join(args['code'])
-                        magic['gregex'] = regex.compile(code.strip())
-                        if args['truncate']:
-                            magic['regex']  = regex.compile(
-                                r"^(?<macro>_?[\w\d]*?):(?<cr>[\r\n]{0,2} {1,16})(?<contents>.*?$)",
-                                flags = regex.DOTALL + regex.MULTILINE
-                            )
-                        else:
-                            magic['regex']  = regex.compile(
-                                r"^(?<macro>_?[\w\d]*?):(?<cr>[\r\n]{0,2} {1,16})(?<contents>.*?$(?:[\r\n]{0,2} {16,16}.*?$)*)",
-                                flags = regex.DOTALL + regex.MULTILINE
-                            )
-                    except:
-                        magic['name']   = ''
-
-                # elif _magic in stata_magics['exit_magics']:
-                #     magic['name']    = _magic
-                #     magic['restart'] = (_magic == 'restart')
-                #     if code.strip() != '':
-                #         magic['name']   = ''
-                #         magic['status'] = -1
-                #         print("Magic %{0} must be called by itself.".format(_magic))
-
-                else:
-                    print("Uknown magic %{0}.".format(_magic))
-
-        elif code.strip().startswith("?"):
-            code = "help " + code.strip()
-
-        if code.strip() == '':
-            magic['status'] = -1
-
-        if (magic['status'] == -1) and :
-            self.quit_early = quit_early
-        else:
-            self.quit_early = None
-
+    def magics(self, code, kernel):
         return code
+
+        # quit_early = {'execution_count': count}
+        # quit_early['status'] = 'ok'
+        # quit_early['payload'] = []
+        # quit_early['user_expressions'] = {}
+        # magic = self.magic_dict
+        #
+        # if code.strip().startswith("%"):
+        #     match = magic_regex.match(code.strip())
+        #     if match:
+        #         _magic = match.groupdict()['magic']
+        #         code   = match.groupdict()['code']
+        #         code   = '' if not code else code.strip()
+        #
+        #         if _magic in stata_magics['plot_magics']:
+        #             magic['name'] = _magic
+        #             try:
+        #                 args = vars(plot_parser.parse_args(code.split(' ')))
+        #                 code = ' '.join(args['code'])
+        #                 args.pop('code', None)
+        #                 args['width']   = args['scale'] * args['width']
+        #                 args['height']  = args['scale'] * args['height']
+        #                 args.pop('scale', None)
+        #                 magic['kwargs'] = args
+        #             except:
+        #                 magic['name']   = ''
+        #                 magic['status'] = -1
+        #
+        #         elif _magic in stata_magics['global_magics']:
+        #             magic['status'] = -1
+        #             magic['name']   = _magic
+        #             magic['locals'] = (_magic == 'locals')
+        #             magic['sregex'] = regex.compile(r"^ {16,16}", flags = regex.MULTILINE)
+        #             try:
+        #                 args = vars(globals_parser.parse_args(code.split(' ')))
+        #                 code = ' '.join(args['code'])
+        #                 magic['gregex'] = regex.compile(code.strip())
+        #                 if args['truncate']:
+        #                     magic['regex']  = regex.compile(
+        #                         r"^(?<macro>_?[\w\d]*?):(?<cr>[\r\n]{0,2} {1,16})(?<contents>.*?$)",
+        #                         flags = regex.DOTALL + regex.MULTILINE
+        #                     )
+        #                 else:
+        #                     magic['regex']  = regex.compile(
+        #                         r"^(?<macro>_?[\w\d]*?):(?<cr>[\r\n]{0,2} {1,16})(?<contents>.*?$(?:[\r\n]{0,2} {16,16}.*?$)*)",
+        #                         flags = regex.DOTALL + regex.MULTILINE
+        #                     )
+        #             except:
+        #                 magic['name']   = ''
+        #
+        #         # elif _magic in stata_magics['exit_magics']:
+        #         #     magic['name']    = _magic
+        #         #     magic['restart'] = (_magic == 'restart')
+        #         #     if code.strip() != '':
+        #         #         magic['name']   = ''
+        #         #         magic['status'] = -1
+        #         #         print("Magic %{0} must be called by itself.".format(_magic))
+        #
+        #         else:
+        #             print("Uknown magic %{0}.".format(_magic))
+        #
+        # elif code.strip().startswith("?"):
+        #     code = "help " + code.strip()
+        #
+        # if code.strip() == '':
+        #     magic['status'] = -1
+        #
+        # if (magic['status'] == -1) and :
+        #     self.quit_early = quit_early
+        # else:
+        #     self.quit_early = None
+        #
+        # return code
 
     def magic_plot(self):
         pass
