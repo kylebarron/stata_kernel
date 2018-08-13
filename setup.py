053005d8f6614977574cd1296640e938d79c97e0
Kyle Barron <barronk@mit.edu>
2018-08-12 23:10:03 -0400

    Move requirements and test_requirements into separate files

--- setup.py
+++ setup.py
@@ -6,27 +6,17 @@ with open('README.md') as f:
 with open('CHANGELOG.md') as history_file:
     history = history_file.read()
 
-requirements = [
-    'jupyter_client>=5.2.3', 'IPython>=6.5.0', 'ipykernel>=4.8.2',
-    'pexpect>=4.6.0;platform_system=="Darwin"',
-    'pexpect>=4.6.0;platform_system=="Linux"', 'python-dateutil>=2.7.3',
-    'pygments>=2.2.0', "regex>=2018.7.11"]
+with open('requirements.txt') as requirements_file:
+    requirements = requirements_file.readlines()
+    requirements = [x[:-1] for x in requirements]
+
+with open('requirements_dev.txt') as test_requirements_file:
+    test_requirements = test_requirements_file.readlines()
+    test_requirements = [x[:-1] for x in test_requirements]
 
 setup_requirements = [
     'setuptools >= 38.6.0',
     'twine >= 1.11.0']
-
-test_requirements = [
-    'mkdocs==1.0',
-    'mkdocs-material==3.0.3',
-    'bumpversion==0.5.3',
-    'coverage==4.5.1',
-    'flake8==3.5.0',
-    'pip==18.0',
-    'pytest==3.7.1',
-    'tox==2.9.1',
-    'wheel==0.30.0',
-    'yapf==0.20.2']
 
 setup(
     author='Kyle Barron',
