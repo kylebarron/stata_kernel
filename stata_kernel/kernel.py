809937fa15d73a10ff34f09e4db12305a748545b
Kyle Barron <barronk@mit.edu>
2018-09-13 23:02:46 -0400

    Copy pygments file to pygments location if newer

--- stata_kernel/kernel.py
+++ stata_kernel/kernel.py
@@ -1,9 +1,13 @@
 import base64
+import shutil
 import platform
 
 from PIL import Image
+from pathlib import Path
 from textwrap import dedent
+from datetime import datetime
 from xml.etree import ElementTree as ET
+from pkg_resources import resource_filename
 from ipykernel.kernelbase import Kernel
 
 from .config import Config
@@ -27,6 +31,22 @@ class StataKernel(Kernel):
     ] # yapf: disable
 
     def __init__(self, *args, **kwargs):
+        # Copy pygments file to Python location
+        pyg_path = Path(resource_filename('pygments', 'lexers/stata.py'))
+        pyg_path_sk = Path(
+            resource_filename('stata_kernel', 'pygments/stata.py'))
+        copy = False
+        if pyg_path.is_file():
+            pyg_path_dt = datetime.fromtimestamp(pyg_path.stat().st_mtime)
+            pyg_path_sk_dt = datetime.fromtimestamp(pyg_path_sk.stat().st_mtime)
+            if pyg_path_sk_dt > pyg_path_dt:
+                copy = True
+        else:
+            copy = True
+
+        if copy:
+            shutil.copy(str(pyg_path_sk), str(pyg_path))
+
         super(StataKernel, self).__init__(*args, **kwargs)
 
         # Can't name this `self.config`. Conflicts with a Jupyter attribute
