35efb5de782cd83b150fdeacaa154cdc4a215b1d
Kyle Barron <barronk@mit.edu>
2018-09-13 22:32:20 -0400

    Write code to history file when run

--- stata_kernel/kernel.py
+++ stata_kernel/kernel.py
@@ -1,3 +1,4 @@
+import os
 import base64
 import platform
 
@@ -66,6 +67,12 @@ class StataKernel(Kernel):
                     'name': 'stderr'})
 
             return {'status': 'error', 'execution_count': self.execution_count}
+
+        # Write line of code to history file
+        if code:
+            r = repr(code)
+            with open(self.conf.get('cache_dir') / 'history.txt', 'a') as f:
+                f.write(r + os.linesep)
 
         # Search for magics in the code
         code = self.magics.magic(code, self)
@@ -202,3 +209,65 @@ class StataKernel(Kernel):
 
     def is_complete(self, code):
         return CodeManager(code, self.sc_delimit_mode).is_complete
+
+    def do_history(hist_access_type, output, raw, session=None, start=None,
+        stop=None, n=None, pattern=None, unique=False):
+
+        pass
+
+# someDictionary = {
+#     helloworld
+# }
+#
+# %%markdown
+# ## Emphasis
+#
+# **This is bold text**
+#
+# __This is bold text__
+#
+# *This is italic text*
+#
+# _This is italic text_
+#
+#
+#
+# ## Test
+#
+# from pathlib import Path
+# path = Path('~/.stata_kernel_cache/history.txt').expanduser()
+#
+# y = reverse_readline(path)
+# [x for x in y]
+#
+# def reverse_readline(filename, buf_size=8192):
+#     """a generator that returns the lines of a file in reverse order"""
+#     with open(filename) as fh:
+#         segment = None
+#         offset = 0
+#         fh.seek(0, os.SEEK_END)
+#         file_size = remaining_size = fh.tell()
+#         while remaining_size > 0:
+#             offset = min(file_size, offset + buf_size)
+#             fh.seek(file_size - offset)
+#             buffer = fh.read(min(remaining_size, buf_size))
+#             remaining_size -= buf_size
+#             lines = buffer.split('\n')
+#             # the first line of the buffer is probably not a complete line so
+#             # we'll save it and append it to the last line of the next buffer
+#             # we read
+#             if segment is not None:
+#                 # if the previous chunk starts right from the beginning of line
+#                 # do not concact the segment to the last line of new chunk
+#                 # instead, yield the segment first
+#                 if buffer[-1] is not '\n':
+#                     lines[-1] += segment
+#                 else:
+#                     yield segment
+#             segment = lines[0]
+#             for index in range(len(lines) - 1, 0, -1):
+#                 if len(lines[index]):
+#                     yield lines[index]
+#         # Don't yield None if the file was empty
+#         if segment is not None:
+#             yield segment
