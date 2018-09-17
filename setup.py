ccdf05dbd82327b8ba3d4718e248603381d18b88
Kyle Barron <barronk@mit.edu>
2018-09-17 00:26:27 -0400

    Bump version: 1.5.0 → 1.5.1

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/kylebarron/stata_kernel',
-    version='1.5.0',
+    version='1.5.1',
     include_package_data=True
 )
