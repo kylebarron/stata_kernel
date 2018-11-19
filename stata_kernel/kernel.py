b0e253a6829588cd589d3cd75988e161c61d51e5
Kyle Barron <barronk@mit.edu>
2018-11-19 14:45:47 -0500

    Bump version: 1.7.0 → 1.7.1

--- stata_kernel/kernel.py
+++ stata_kernel/kernel.py
@@ -22,7 +22,7 @@ from .stata_magics import StataMagics
 
 class StataKernel(Kernel):
     implementation = 'stata_kernel'
-    implementation_version = '1.7.0'
+    implementation_version = '1.7.1'
     language = 'stata'
     language_info = {
         'name': 'stata',
