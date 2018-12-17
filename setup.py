46f2737ed6978252b2957b4357df88350a2c3cdf
Kyle Barron <barronk@mit.edu>
2018-12-17 12:48:29 -0500

    Bump version: 1.8.0 → 1.8.1

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/kylebarron/stata_kernel',
-    version='1.8.0',
+    version='1.8.1',
     include_package_data=True
 )
--- stata_kernel/__init__.py
+++ stata_kernel/__init__.py
@@ -1,5 +1,5 @@
 """An example Jupyter kernel"""
 
-__version__ = '1.8.0'
+__version__ = '1.8.1'
 
 from .kernel import StataKernel
