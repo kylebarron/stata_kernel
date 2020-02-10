45bcf9e1ed2fe181e0bbe373f6a2bfbe406da1c6
Kyle Barron <barronk@mit.edu>
2020-02-10 11:05:33 -0500

    Bump version: 1.10.6 → 1.11.0

--- stata_kernel/__init__.py
+++ stata_kernel/__init__.py
@@ -1,5 +1,5 @@
 """An example Jupyter kernel"""
 
-__version__ = '1.10.6'
+__version__ = '1.11.0'
 
 from .kernel import StataKernel
--- stata_kernel/kernel.py
+++ stata_kernel/kernel.py
@@ -22,7 +22,7 @@ from .stata_magics import StataMagics
 
 class StataKernel(Kernel):
     implementation = 'stata_kernel'
-    implementation_version = '1.10.6'
+    implementation_version = '1.11.0'
     language = 'stata'
     language_info = {
         'name': 'stata',
