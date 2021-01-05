a8d9f3bdf61a3ff15ae29f5ab056ff1994cbecf4
Mauricio Caceres <mauricio.caceres.bravo@gmail.com>
2021-01-05 14:54:12 -0500

    Added stack trace to kernel import

--- stata_kernel/__init__.py
+++ stata_kernel/__init__.py
@@ -1,8 +1,11 @@
 """An example Jupyter kernel"""
 
+import traceback
+
 __version__ = '1.12.1'
 
 try:
     from .kernel import StataKernel
 except:
     print('Cannot import kernel')
+    traceback.print_exc()
