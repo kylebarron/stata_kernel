104ca67d50e3860ff2215775c83bc7623157f901
Mauricio Caceres <mauricio.caceres.bravo@gmail.com>
2021-05-09 10:44:00 -0400

    Bump version: 1.12.3 → 1.13.0

--- setup.py
+++ setup.py
@@ -59,6 +59,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/kylebarron/stata_kernel',
-    version='1.12.3',
+    version='1.13.0',
     include_package_data=True
 )
