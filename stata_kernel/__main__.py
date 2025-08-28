38f4b1f4c05074d5c791d0938c7d11d591d09dfe
Daniel Da Cunha <daniel@ddc.im>
2025-08-28 11:00:13 +0900

    Improve loggin of errors by outputting the underlying error message to stderr (#442)

--- stata_kernel/__main__.py
+++ stata_kernel/__main__.py
@@ -3,8 +3,8 @@ from ipykernel.kernelapp import IPKernelApp
 import traceback
 try:
     from .kernel import StataKernel
-except:
-    print('Cannot import kernel')
+except Exception as e:
+    print('Error while importing StataKernel:', e)
     traceback.print_exc()
 
 IPKernelApp.launch_instance(kernel_class=StataKernel)
