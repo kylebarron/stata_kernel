862c860487d59c392f8d70f6542a81488b5ee1c8
Mauricio Caceres Bravo <mauricio.caceres.bravo@gmail.com>
2025-08-27 22:58:27 -0400

    Add readme for poetry publish (#445)
    * Manually bumped to 1.12.3
    
    * Bump version: 1.12.3 → 1.13.0
    
    * Specified readme in pyproject.toml
    
    * Debugging tags in poetry
    
    * Debugging tags in poetry
    
    * bump version 1.14.1 → 1.14.2

--- stata_kernel/__init__.py
+++ stata_kernel/__init__.py
@@ -1,3 +1,3 @@
 """An example Jupyter kernel"""
 
-__version__ = '1.14.1'
+__version__ = '1.14.2'
--- stata_kernel/kernel.py
+++ stata_kernel/kernel.py
@@ -22,7 +22,7 @@ from .stata_magics import StataMagics
 
 class StataKernel(Kernel):
     implementation = 'stata_kernel'
-    implementation_version = '1.14.1'
+    implementation_version = '1.14.2'
     language = 'stata'
     language_info = {
         'name': 'stata',
